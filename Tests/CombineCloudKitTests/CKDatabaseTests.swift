40facb03076f56236e49d95ecf3187badd0c9ed9
Chris Araman <chris-araman@users.noreply.github.com>
2021-02-16 17:06:14 -0800

    Tests: empty implementation

--- Tests/CombineCloudKitTests/CKDatabaseTests.swift
+++ Tests/CombineCloudKitTests/CKDatabaseTests.swift
@@ -4,13 +4,5 @@ import XCTest
 
 final class CKDatabaseTests: XCTestCase {
   func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(CombineCloudKit().text, "Hello, World!")
   }
-
-  static var allTests = [
-    ("testExample", testExample)
-  ]
 }
