d2044562c0893484ccb1e8e438dcc6cf69b32baa
Chris Araman <chris-araman@users.noreply.github.com>
2021-02-18 17:25:30 -0800

    CKContainer: handle accountStatus

---
+++ Sources/CombineCloudKit/CKContainer.swift
@@ -0,0 +1,25 @@
+//
+//  CKContainer.swift
+//  CombineCloudKit
+//
+//  Created by Chris Araman on 2/18/21.
+//  Copyright © 2021 Chris Araman. All rights reserved.
+//
+
+import CloudKit
+import Combine
+
+extension CKContainer {
+  public func accountStatus() -> Future<CKAccountStatus, Error> {
+    Future { promise in
+      self.accountStatus { status, error in
+        guard error == nil else {
+          promise(.failure(error!))
+          return
+        }
+
+        promise(.success(status))
+      }
+    }
+  }
+}
