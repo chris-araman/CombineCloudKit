8ff8040119ff3e652974dc6cb25508c53a9803e5
Chris Araman <chris-araman@users.noreply.github.com>
2021-02-18 22:52:39 -0800

    CKRecordZone: make modify return two publishers

--- Sources/CombineCloudKit/CKDatabase/CKRecordZone.swift
+++ Sources/CombineCloudKit/CKDatabase/CKRecordZone.swift
@@ -111,31 +111,54 @@ extension CKDatabase {
 
     return subject.propagateCancellationTo(operation)
   }
+  
+  public struct CCKModifyRecordZonePublishers {
+    let saved: AnyPublisher<CKRecordZone, Error>
+    let deleted: AnyPublisher<CKRecordZone.ID, Error>
+  }
 
   public func modify(
     recordZonesToSave: [CKRecordZone]? = nil,
     recordZoneIDsToDelete: [CKRecordZone.ID]? = nil,
     withConfiguration configuration: CKOperation.Configuration? = nil
-  ) -> AnyPublisher<([CKRecordZone]?, [CKRecordZone.ID]?), Error> {
+  ) -> CCKModifyRecordZonePublishers {
+    let savedSubject = PassthroughSubject<CKRecordZone, Error>()
+    let deletedSubject = PassthroughSubject<CKRecordZone.ID, Error>()
     let operation = CKModifyRecordZonesOperation(
       recordZonesToSave: recordZonesToSave, recordZoneIDsToDelete: recordZoneIDsToDelete
     )
     if configuration != nil {
       operation.configuration = configuration
     }
-
-    return Future { promise in
-      operation.modifyRecordZonesCompletionBlock = { saved, deleted, error in
-        guard error == nil else {
-          promise(.failure(error!))
-          return
+    operation.modifyRecordZonesCompletionBlock = { saved, deleted, error in
+      guard error == nil else {
+        savedSubject.send(completion: .failure(error!))
+        deletedSubject.send(completion: .failure(error!))
+        return
+      }
+      
+      if let saved = saved {
+        for record in saved {
+          savedSubject.send(record)
         }
-
-        promise(.success((saved, deleted)))
       }
 
-      self.add(operation)
-    }.propagateCancellationTo(operation)
+      if let deleted = deleted {
+        for recordID in deleted {
+          deletedSubject.send(recordID)
+        }
+      }
+
+      savedSubject.send(completion: .finished)
+      deletedSubject.send(completion: .finished)
+    }
+
+    add(operation)
+    
+    return CCKModifyRecordZonePublishers(
+      saved: savedSubject.propagateCancellationTo(operation),
+      deleted: deletedSubject.propagateCancellationTo(operation)
+    )
   }
 
   public func fetchAtBackgroundPriority(
