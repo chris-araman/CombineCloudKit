9de67f116d13daf154cb6effa695a96432bdf4ad
Chris Araman <chris-araman@users.noreply.github.com>
2021-03-04 18:14:09 -0800

    CKRecord: document public functions

--- Sources/CombineCloudKit/CKDatabase/CKRecord.swift
+++ Sources/CombineCloudKit/CKDatabase/CKRecord.swift
@@ -10,10 +10,20 @@ import CloudKit
 import Combine
 
 extension CKDatabase {
+  /// Saves a single record.
+  ///
+  /// CombineCloudKit executes the save with a low priority. Use this method when you don’t require the save to happen
+  /// immediately.
+  /// - Returns: A `Publisher` that emits the saved `CKRecord`, or an error if CombineCloudKit can't save it.
+  /// - SeeAlso: [`save`](https://developer.apple.com/documentation/cloudkit/ckdatabase/1449114-save)
   public final func saveAtBackgroundPriority(record: CKRecord) -> AnyPublisher<CKRecord, Error> {
     publisherFrom(save, with: record)
   }
 
+  /// Saves a single record.
+  ///
+  /// - Returns: A `Publisher` that emits the saved `CKRecord`, or an error if CombineCloudKit can't save it.
+  /// - SeeAlso: [`CKModifyRecordsOperation`](https://developer.apple.com/documentation/cloudkit/ckmodifyrecordsoperation)
   public final func save(
     record: CKRecord,
     withConfiguration configuration: CKOperation.Configuration? = nil
@@ -21,6 +31,10 @@ extension CKDatabase {
     save(records: [record], withConfiguration: configuration)
   }
 
+  /// Saves multiple records.
+  ///
+  /// - Returns: A `Publisher` that emits the saved `CKRecord`s, or an error if CombineCloudKit can't save them.
+  /// - SeeAlso: [`CKModifyRecordsOperation`](https://developer.apple.com/documentation/cloudkit/ckmodifyrecordsoperation)
   public final func save(
     records: [CKRecord],
     atomically isAtomic: Bool = true,
@@ -35,12 +49,23 @@ extension CKDatabase {
     return publishers.saved.propagateCancellationTo(operation)
   }
 
+  /// Deletes a single record.
+  ///
+  /// CombineCloudKit executes the delete with a low priority. Use this method when you don’t require the delete to
+  /// happen immediately.
+  /// - Returns: A `Publisher` that emits the saved `CKRecord.ID`, or an error if CombineCloudKit can't save it.
+  /// - SeeAlso: [`delete`](https://developer.apple.com/documentation/cloudkit/ckdatabase/1449122-delete)
   public final func deleteAtBackgroundPriority(recordID: CKRecord.ID)
     -> AnyPublisher<CKRecord.ID, Error>
   {
     publisherFrom(delete, with: recordID)
   }
 
+  /// Deletes a single record.
+  ///
+  /// - Returns: A `Publisher` that emits the deleted `CKRecord.ID`, or an error if CombineCloudKit can't delete
+  /// it.
+  /// - SeeAlso: [`CKModifyRecordsOperation`](https://developer.apple.com/documentation/cloudkit/ckmodifyrecordsoperation)
   public final func delete(
     recordID: CKRecord.ID,
     withConfiguration configuration: CKOperation.Configuration? = nil
@@ -48,6 +73,11 @@ extension CKDatabase {
     delete(recordIDs: [recordID], withConfiguration: configuration)
   }
 
+  /// Deletes multiple records.
+  ///
+  /// - Returns: A `Publisher` that emits the deleted `CKRecord.ID`s, or an error if CombineCloudKit can't delete
+  /// them.
+  /// - SeeAlso: [`CKModifyRecordsOperation`](https://developer.apple.com/documentation/cloudkit/ckmodifyrecordsoperation)
   public final func delete(
     recordIDs: [CKRecord.ID],
     atomically isAtomic: Bool = true,
@@ -62,12 +92,19 @@ extension CKDatabase {
     return publishers.deleted.propagateCancellationTo(operation)
   }
 
+  /// `Publisher`s returned by `modify`.
+  ///
+  /// Canceling either `Publisher` cancels the underlying `CKModifyRecordsOperation`.
   public struct CCKModifyRecordsPublishers {
     let progress: AnyPublisher<(CKRecord, Double), Error>
     let saved: AnyPublisher<CKRecord, Error>
     let deleted: AnyPublisher<CKRecord.ID, Error>
   }
 
+  /// Modifies one or more records.
+  ///
+  /// - Returns: A `CCKModifyRecordsPublishers`.
+  /// - SeeAlso: [`CKModifyRecordsOperation`](https://developer.apple.com/documentation/cloudkit/ckmodifyrecordsoperation)
   public final func modify(
     recordsToSave: [CKRecord]? = nil,
     recordIDsToDelete: [CKRecord.ID]? = nil,
@@ -145,24 +182,42 @@ extension CKDatabase {
     )
   }
 
+  /// Fetches the record with the specified ID.
+  ///
+  /// CombineCloudKit executes the fetch with a low priority. Use this method when you don’t require the record
+  /// immediately.
+  /// - Returns: A `Publisher` that emits the `CKRecord`, or an error if CombineCloudKit can't fetch it.
+  /// - SeeAlso: [fetch](https://developer.apple.com/documentation/cloudkit/ckdatabase/1449126-fetch)
   public final func fetchAtBackgroundPriority(
     withRecordID recordID: CKRecord.ID
   ) -> AnyPublisher<CKRecord, Error> {
     publisherFrom(fetch, with: recordID)
   }
 
+  /// `Publisher`s returned by `fetch`.
+  ///
+  /// Canceling either `Publisher` cancels the underlying `CKFetchRecordsOperation`.
   public struct CCKFetchRecordsPublishers {
     let progress: AnyPublisher<(CKRecord.ID, Double), Error>
     let fetched: AnyPublisher<CKRecord, Error>
   }
 
+  /// Fetches the record with the specified ID.
+  ///
+  /// - Returns: A `CCKFetchRecordsPublishers`.
+  /// - SeeAlso: [CKFetchRecordsOperation](https://developer.apple.com/documentation/cloudkit/ckfetchrecordsoperation)
   public final func fetch(
     recordID: CKRecord.ID,
+    desiredKeys: [CKRecord.FieldKey]? = nil,
     withConfiguration configuration: CKOperation.Configuration? = nil
   ) -> CCKFetchRecordsPublishers {
-    fetch(recordIDs: [recordID], withConfiguration: configuration)
+    fetch(recordIDs: [recordID], desiredKeys: desiredKeys, withConfiguration: configuration)
   }
 
+  /// Fetches multiple records.
+  ///
+  /// - Returns: A `CCKFetchRecordsPublishers`.
+  /// - SeeAlso: [CKFetchRecordsOperation](https://developer.apple.com/documentation/cloudkit/ckfetchrecordsoperation)
   public final func fetch(
     recordIDs: [CKRecord.ID],
     desiredKeys: [CKRecord.FieldKey]? = nil,
@@ -206,6 +261,10 @@ extension CKDatabase {
     )
   }
 
+  /// Fetches the current user record.
+  ///
+  /// - Returns: A `Publisher` that emits the `CKRecord`, or an error if CombineCloudKit can't fetch it.
+  /// - SeeAlso: [fetchCurrentUserRecordOperation](https://developer.apple.com/documentation/cloudkit/ckfetchrecordsoperation/1476070-fetchcurrentuserrecordoperation)
   public final func fetchCurrentUserRecord(
     desiredKeys _: [CKRecord.FieldKey]? = nil,
     withConfiguration configuration: CKOperation.Configuration? = nil
@@ -216,6 +275,10 @@ extension CKDatabase {
     }
   }
 
+  /// Performs a query.
+  ///
+  /// - Returns: A `Publisher` that emits any matching `CKRecord`s, or an error if CombineCloudKit can't perform the query.
+  /// - SeeAlso: [CKQueryOperation](https://developer.apple.com/documentation/cloudkit/ckqueryoperation)
   public final func performQuery(
     ofType recordType: CKRecord.RecordType,
     where predicate: NSPredicate = NSPredicate(value: true),
@@ -234,6 +297,10 @@ extension CKDatabase {
     )
   }
 
+  /// Performs a query.
+  ///
+  /// - Returns: A `Publisher` that emits any matching `CKRecord`s, or an error if CombineCloudKit can't perform the query.
+  /// - SeeAlso: [CKQueryOperation](https://developer.apple.com/documentation/cloudkit/ckqueryoperation)
   public final func perform(
     query: CKQuery,
     inZoneWith zoneID: CKRecordZone.ID? = nil,
