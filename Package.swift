50c82c90fab099bd805e3f714de05021d30c7072
Chris Araman <chris-araman@users.noreply.github.com>
2021-06-01 16:29:27 -0700

    Package: CombineExpectations 0.8.0

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/groue/CombineExpectations.git", from: "0.7.0")
+    .package(url: "https://github.com/groue/CombineExpectations.git", from: "0.8.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
