d7e1fda4776838504807ea9da903fa9e328d804a
Chris Araman <chris@hiddenplace.dev>
2021-06-16 02:35:37 -0700

    ErrorInjectionTests: test error propagation from fetchAll publishers

--- Tests/CombineCloudKitTests/ErrorInjectionTests.swift
+++ Tests/CombineCloudKitTests/ErrorInjectionTests.swift
@@ -22,6 +22,65 @@ import XCTest
       }
     }
 
+    func testFetchAllRecordZones() throws {
+      try validateFetchAll(
+        (1...3).map { CKRecordZone(zoneName: "\($0)") },
+        { $0.save },
+        { database in { database.fetchAllRecordZones() } }
+      )
+    }
+
+    func testFetchAllRecordZonesAtBackgroundPriority() throws {
+      try validateFetchAll(
+        (1...3).map { CKRecordZone(zoneName: "\($0)") },
+        { $0.save },
+        { $0.fetchAllRecordZonesAtBackgroundPriority }
+      )
+    }
+
+    func testFetchCurrentUserRecord() throws {
+      try verifyErrorPropagation() { _, database in
+        let userRecord = CKRecord(
+          recordType: "CurrentUserRecord", recordID: MockOperationFactory.currentUserRecordID)
+        let save = database.save(record: userRecord)
+        try wait(for: \.finished, from: save)
+
+        let desiredKeys = ["Key"]
+        let fetch = database.fetchCurrentUserRecord(desiredKeys: desiredKeys)
+        return try wait(for: \.completion, from: fetch)
+      }
+    }
+
+    func testFetchAllSubscriptions() throws {
+      try validateFetchAll(
+        (1...3).map { CKDatabaseSubscription(subscriptionID: "\($0)") },
+        { $0.save },
+        { database in { database.fetchAllSubscriptions() } }
+      )
+    }
+
+    func testFetchAllSubscriptionsAtBackgroundPriority() throws {
+      try validateFetchAll(
+        (1...3).map { CKDatabaseSubscription(subscriptionID: "\($0)") },
+        { $0.save },
+        { $0.fetchAllSubscriptionsAtBackgroundPriority }
+      )
+    }
+
+    private func validateFetchAll<T>(
+      _ items: [T],
+      _ save: (CCKDatabase) -> (([T], CKOperation.Configuration?) -> AnyPublisher<T, Error>),
+      _ fetch: (CCKDatabase) -> (() -> AnyPublisher<T, Error>)
+    ) throws where T: Hashable {
+      try verifyErrorPropagation() { _, database in
+        let save = save(database)(items, nil)
+        try wait(for: \.finished, from: save)
+
+        let fetch = fetch(database)()
+        return try wait(for: \.completion, from: fetch)
+      }
+    }
+
     func testQueryPropagatesErrors() throws {
       try verifyErrorPropagation { _, database in
         let record = CKRecord(recordType: "Test")
