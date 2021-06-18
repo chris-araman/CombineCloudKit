da1029478095ec0d30c233e60556acc07d7b5d73
Chris Araman <chris@hiddenplace.dev>
2021-06-18 16:17:29 -0700

    Package: clarify watchOS requirements

--- Package.swift
+++ Package.swift
@@ -22,12 +22,20 @@ let package = Package(
   swiftLanguageVersions: [.v5]
 )
 
-#if swift(>=5.2)
-  // CombineCloudKitTests require Swift 5.2.
-  package.platforms += [
-    // XCTest requires watchOS 7.4.
-    .watchOS("7.4"),
+#if swift(>=5.4)
+  // XCTest for watchOS requires watchOS 7.4 and Swift 5.4.
+  package.platforms! += [
+    .watchOS("7.4")
   ]
+#else
+  // Combine requires watchOS 6.
+  package.platforms! += [
+    .watchOS(.v6)
+  ]
+#endif
+
+// CombineCloudKitTests require Swift 5.2.
+#if swift(>=5.2)
   package.dependencies += [
     // Test improvements for Xcode 13 beta.
     .package(url: "https://github.com/groue/CombineExpectations.git", .branch("linker"))
@@ -39,12 +47,7 @@ let package = Package(
         "CombineCloudKit",
         "CombineExpectations",
       ]
-    ),
-  ]
-#else
-  // Combine requires watchOS 6.
-  package.platforms += [
-    .watchOS(v6),
+    )
   ]
 #endif
 
@@ -54,4 +57,3 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-format", .branch("swift-5.4-branch"))
   ]
 #endif
-
