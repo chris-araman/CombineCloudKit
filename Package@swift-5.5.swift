9525553d8f241a5070e393bd579da9bc7336b892
Chris Araman <chris@hiddenplace.dev>
2021-06-19 16:30:51 -0700

    Package: initial support for Mac Catalyst

---
+++ Package@swift-5.5.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+  name: "CombineCloudKit",
+  // Combine requires macOS 10.15, Mac Catalyst 13, iOS 13, tvOS 13, or watchOS 6.
+  // XCTest requires watchOS 7.4.
+  platforms: [
+    .macCatalyst(.v13),
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .tvOS(.v13),
+    .watchOS("7.4"),
+  ],
+  products: [
+    .library(
+      name: "CombineCloudKit",
+      targets: ["CombineCloudKit"]
+    )
+  ],
+  dependencies: [
+    // Test improvements for Xcode 13 beta.
+    .package(url: "https://github.com/groue/CombineExpectations.git", .branch("linker"))
+  ],
+  targets: [
+    .target(name: "CombineCloudKit"),
+    .testTarget(
+      name: "CombineCloudKitTests",
+      dependencies: [
+        "CombineCloudKit",
+        "CombineExpectations",
+      ]
+    ),
+  ],
+  swiftLanguageVersions: [.v5]
+)
